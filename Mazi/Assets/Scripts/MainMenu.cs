034987fef7bbb40d82f3027df1433880b5060376
Kershawn <kershawn7700@gmail.com>
2021-03-04 15:56:43 -0400

    Added Arena Scene and Started  a Card Prefab

--- Mazi/Assets/Scripts/MainMenu.cs
+++ Mazi/Assets/Scripts/MainMenu.cs
@@ -12,13 +12,13 @@ public class MainMenu : MonoBehaviour
 
 	}
 
-    public void searchGame()
-    {
-
-    }
-
     public void manageDecks ()
     {
     	SceneManager.LoadScene(1);
+    }
+
+    public void searchGame()
+    {
+        SceneManager.LoadScene(2);
     }
 }
