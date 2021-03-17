7d3d459c790112de2686a589b1326bcc8112f70e
Dion Recai <dijereo909@gmail.com>
2021-03-17 11:04:48 -0400

    Selector concept working

---
+++ Mazi/Assets/Scripts/SwitchSelectTry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSelectTry : MonoBehaviour
+{
+    public OnClickTry cube0;
+    public OnClickTry cube1;
+    public OnClickTry cube2;
+
+    public void selectCube(OnClickTry selectedCube)
+    {
+    	cube0.SetDeselectMaterial();
+    	cube1.SetDeselectMaterial();
+    	cube2.SetDeselectMaterial();
+    	selectedCube.SetSelectMaterial();
+    }
+}
