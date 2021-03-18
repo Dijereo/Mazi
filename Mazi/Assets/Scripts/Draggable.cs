7343d0143a7709caf8cc5c1373ff8156c6b45dee
Kershawn <kershawn7700@gmail.com>
2021-03-18 16:19:10 -0400

    Implemented drag functionality on cards and drop area functionality

--- Mazi/Assets/Scripts/Draggable.cs
+++ Mazi/Assets/Scripts/Draggable.cs
@@ -1,9 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Draggable : MonoBehaviour
+public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    [SerializeField] private Canvas canvas;
+    public string m_text;
+    private RectTransform RectTransform;
+    private CanvasGroup canvasGroup;
+    private Transform parentToReturnTo = null;
+
+    public void Awake(){
+
+        RectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        
+    }
+    public void OnBeginDrag(PointerEventData eventData){
+        Debug.Log("OnBeginDrag");
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData){
+        Debug.Log("OnDrag");
+        RectTransform.anchoredPosition += eventData.delta/70;
+        //this.transform.position = eventData.position;
+        m_text = "Position:" + this.transform.position + "Mouse: " + eventData.position;
+        //m_text = "Mouse position: " + eventData.position + "Card Position: " + this.position;
+        Debug.Log(m_text);
+        Debug.Log("   " + canvas.scaleFactor);
+    }
+
+    public void OnEndDrag(PointerEventData eventData){
+        Debug.Log("OnEndDrag");
+        canvasGroup.blocksRaycasts = true;
+        this.transform.SetParent(parentToReturnTo);
+    }
+
+    public void OnPointerDown(PointerEventData eventData){
+        Debug.Log("OnPointerDown");
+    }
     
+    public void setParentToReturnTo(Transform t){
+
+        this.parentToReturnTo = t;
+    }
     
 }
